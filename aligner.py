b3711c889174ffec12ffbff9985d4ddeda886163
AA Ron <aaronzira@gmail.com>
2017-04-27 16:04:39 -0700

    temporary push

--- aligner.py
+++ aligner.py
@@ -1,4 +1,5 @@
 import os
+import re
 import multiprocessing
 import json
 import subprocess
@@ -10,11 +11,13 @@ def data_generator(file_id):
 
     # input sources
     txt_file = "~/records/{}.txt".format(file_id)
-    audio_file = "./{}.mp3".format(file_id))
+    audio_file = "./{}.mp3".format(file_id)
 
     # grab audio file
     bucket = boto3.resource("s3").Bucket("cgws")
     bucket.download_file("{}.mp3".format(file_id),audio_file)
+
+    ## convert to wav file ##
 
     # output
     text_out_dir = "~/deepspeech_data/stm"
@@ -22,10 +25,17 @@ def data_generator(file_id):
 
     # reading json from alignment using gentle
     with open(txt_file,"r") as tr:
-        transcript = tr.read()
+        transcription = tr.read()
+
+        transcription = re.sub("\d:\d+:\d+\.\d S(\d+|\?): ","",transcription)
+        transcription = re.sub("\[.+?\]","",transcription)
+        transcription = re.sub("\-"," ",transcription)
+        transcription = re.sub("\s{2,}"," ",transcription)
+        transcription = re.sub(r"[^a-zA-Z0-9\' ]","",transcription,re.UNICODE)
+
     resources = gentle.Resources()
     with gentle.resampled(audio_file) as wavfile:
-        aligner = gentle.ForcedAligner(resources,transcript,nthreads=multiprocessing.cpu_count(),
+        aligner = gentle.ForcedAligner(resources,transcription,nthreads=multiprocessing.cpu_count(),
                                        disfluency=False,conservative=False,disfluencies=set(["uh","um"]))
         result = aligner.transcribe(wavfile)
     # now a dictionary
@@ -54,6 +64,7 @@ def data_generator(file_id):
                 start_time = catch["start"]
                 times.append(start_time)
                 current = catch["alignedWord"]
+                end_time = catch["end"]
 
             # append captured word to current string if total time is under 20s
             else:
@@ -61,7 +72,7 @@ def data_generator(file_id):
                 end_time = catch["end"]
 
         # miss after prior success(es)
-        elif catch["case"] != "success" and current:
+        elif (catch["case"] != "success" or catch["alignedWord"] == "<unk>") and current:
             strings.append(current)
             times.append(end_time)
             current = ""
@@ -82,8 +93,9 @@ def data_generator(file_id):
     # zip start, end, and duration of each string
     start_end_duration = []
     for start,end in zip(times[::2],times[1::2]):
-        start_end_duration.append((float("{:.2f}".format(start)),
-                                   float("{:.2f}".format(end)),
+        start_end_duration.append((
+                                   int(start*100),
+                                   int(end*100),
                                    float("{:.2f}".format(end-start))))
 
     # zip strings and times
@@ -91,10 +103,12 @@ def data_generator(file_id):
 
     # write strings to text files and split mp3 file into consituent wav files
     for text,(start,end,duration) in text_and_times:
+        ## remove decimals ##
         with open(os.path.join(text_out_dir,"{}_{}_{}.txt".format(file_id,start,end)),"w") as f:
             f.write(text)
 
         audio_segment = "{}_{}_{}.wav".format(os.path.join(wav_out_dir,file_id),start,end)
+        ## use wav file here instead ##
         subprocess.call(["sox","{}".format(audio_file),"-r","16k",
                         "{}".format(audio_segment),"trim","{}".format(start),
                         "{}".format(duration),"remix","-"])
